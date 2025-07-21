e772e6939dad0cef2b463743aa42d5e09ebc4756
aabbey-mtn-gh <aabbey@orangetech.io>
2025-07-21 07:45:34 +0000

    added jsconfig eslintrc

--- sample-project-wdio/test/specs/ios/ios-native.spec.js
+++ sample-project-wdio/test/specs/ios/ios-native.spec.js
@@ -1,3 +1,5 @@
+import {$, $$, driver, expect} from '@wdio/globals';
+
 describe('IOS Native Features', () =>{
     it('Working with alert box', async() => {
         await $('~Alert Views').click();
@@ -6,12 +8,51 @@ describe('IOS Native Features', () =>{
         //click ok
         await $('~OK').click();
 
+        //Get the text of the alert 
+        console.log(await driver.getAlertText());
+
         //accept/dismiss alert
-        await driver.dismissAlert();
+        // await driver.dismissAlert();
         await driver.acceptAlert();
 
         //assertion
         await expect($('~OK')).not.toExist();
+    });
+
+    it ('Working with scrollable element', async() => {
+
+        //easiest
+        await driver.execute('mobile: scroll', {direction: "down"});  //direction down
+        await driver.execute('mobile: scroll', {direction: "up"}); //direction up
+
+        //complex scenario
+        const redPicker = await $('~Red color component value');
+        const bluePicker = await $('~Blue color component value');
+
+        await $('~Picker View').click()
+        await driver.execute('mobile: scroll', {element: redPicker.elementId ,direction: "down"});
+        await driver.execute('mobile: scroll', {element: bluePicker.elementId ,direction: "up"});
+
+       
+    });
+
+
+    it('Working with picker view', async() => {
+
+        //easiest
+        //await driver.execute('mobile: scroll', {direction: "down"});  //direction down
+        //await driver.execute('mobile: scroll', {direction: "up"}); //direction up
+
+        //complex scenario - most preferred in such scenario
+        const redPicker = await $('~Red color component value');
+        const bluePicker = await $('~Blue color component value');
+        const greenPicker = await $('~Green color component value');
+
+        await $('~Picker View').click()
+        await redPicker.addValue('125');
+        await bluePicker.addValue('125');
+        await greenPicker.addValue('0');
+
     });
 
     
